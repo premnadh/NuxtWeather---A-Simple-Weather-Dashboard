ed52feba6663dcdce69b4d77e14f29a797561150
premnadh <103254598+premnadh@users.noreply.github.com>
2025-04-23 19:33:32 +0530

    Create WeatherCard.vue

---
+++ components/WeatherCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-xl font-bold">{{ weather.name }}</h2>
+    <p class="text-4xl">{{ weather.main.temp }}°C</p>
+    <p>{{ weather.weather[0].description }}</p>
+    <p>Humidity: {{ weather.main.humidity }}%</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['weather'])
+</script>
