75090aa84d2169183cbfe6a049dab8683069dae2
premnadh <103254598+premnadh@users.noreply.github.com>
2025-04-23 19:37:14 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.{js,vue,ts}',
+    './pages/**/*.{js,vue,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
